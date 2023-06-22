fbb91cbe98f1a09978e1c3733fd0ca51e11b568f
Lucas Pereira <lucasbarbosaj.pereira@gmail.com>
2023-06-22 10:11:58 -0300

    finalizando tela do search cinema

--- lib/search_cinema.dart
+++ lib/search_cinema.dart
@@ -15,10 +15,12 @@ class SearchCinema extends StatefulWidget {
 
 class _SearchCinemaState extends State<SearchCinema> {
   List<dynamic> cinemasData = [];
+  List<dynamic> filteredCinemasData = [];
   int? selectedCinemaId;
   SharedPreferences? localStorage;
   String? selectedRegion;
   List<dynamic> regionsData = [];
+  TextEditingController searchController = TextEditingController();
 
   @override
   void initState() {
@@ -26,6 +28,23 @@ class _SearchCinemaState extends State<SearchCinema> {
     loadCinemasData();
     loadRegionsData();
     initializeSharedPreferences();
+    searchController.addListener(filterCinemas);
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    super.dispose();
+  }
+
+  void filterCinemas() {
+    String searchText = searchController.text.trim().toLowerCase();
+    setState(() {
+      filteredCinemasData = cinemasData.where((cinema) {
+        String cinemaName = cinema['name'].toLowerCase();
+        return cinemaName.contains(searchText);
+      }).toList();
+    });
   }
 
   Future<void> loadCinemasData() async {
@@ -33,6 +52,7 @@ class _SearchCinemaState extends State<SearchCinema> {
     final data = jsonDecode(jsonString);
     setState(() {
       cinemasData = data['cinemas'];
+      filteredCinemasData = cinemasData;
     });
   }
 
@@ -58,9 +78,16 @@ class _SearchCinemaState extends State<SearchCinema> {
     });
   }
 
-  void selectRegion(String? regionName) {
+  void selectRegion(
+    int? region,
+  ) {
     setState(() {
-      selectedRegion = regionName;
+      regionsData.map((regiao) => {
+            if (regiao['id'] == region) {selectedRegion = regiao['name']}
+          });
+      selectedRegion;
+      filteredCinemasData =
+          cinemasData.where((cinema) => cinema['regiao'] == region).toList();
     });
   }
 
@@ -86,8 +113,9 @@ class _SearchCinemaState extends State<SearchCinema> {
                   ),
                 ),
               ),
-              DropdownButton<String>(
-                value: selectedRegion,
+              DropdownButton<int>(
+                value:
+                    selectedRegion != null ? int.parse(selectedRegion!) : null,
                 hint: const Text('Selecione uma região'),
                 dropdownColor: const Color(0xFF111111),
                 style: const TextStyle(
@@ -98,17 +126,17 @@ class _SearchCinemaState extends State<SearchCinema> {
                 ),
                 items: regionsData.map((region) {
                   final String regionName = region['name'];
-                  return DropdownMenuItem<String>(
-                    value: regionName,
+                  final int regionId = region['id'];
+                  return DropdownMenuItem<int>(
+                    value: regionId,
                     child: Text(regionName),
                   );
                 }).toList(),
                 onChanged: (newValue) {
                   setState(() {
-                    //set state will update UI and State of your App
-                    selectedRegion = newValue.toString();
+                    selectedRegion = newValue?.toString();
+                    selectRegion(newValue);
                   });
-                  selectRegion(newValue);
                   Navigator.pop(context);
                   openRegionModal(context);
                 },
@@ -139,7 +167,38 @@ class _SearchCinemaState extends State<SearchCinema> {
                 padding: const EdgeInsets.all(22.0),
                 child: Column(
                   children: [
-                    const Pesquisa(),
+                    TextField(
+                      controller: searchController,
+                      style: const TextStyle(
+                        fontFamily: 'Inter',
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                        color: Color(0xFFD9D9D9), // Cor do texto digitado
+                      ),
+                      decoration: InputDecoration(
+                        filled: false,
+                        hintText: 'Pesquisar',
+                        suffixIcon: const Icon(Icons.search),
+                        suffixIconColor: const Color(0x7CD9D9D9),
+                        hintStyle: const TextStyle(
+                          color: Color(0x7CD9D9D9), // Cor do hintText
+                        ),
+                        contentPadding: const EdgeInsets.symmetric(
+                            vertical: 12, horizontal: 16),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: const BorderSide(
+                            color: Color(0xFFD9D9D9),
+                          ), // Cor da borda quando não está em foco
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: const BorderSide(
+                              color:
+                                  Color(0xFFD9D9D9)), // Cor da borda ao focar
+                        ),
+                      ),
+                    ),
                     const SizedBox(
                       height: 10,
                     ),
@@ -165,107 +224,135 @@ class _SearchCinemaState extends State<SearchCinema> {
                       height: 30,
                     ),
                     Column(
-                      children: cinemasData.map((cinema) {
-                        final int cinemaId = cinema['id'];
-                        final String name = cinema['name'];
-                        final String address = cinema['endereco'];
-                        final String imageUrl = cinema['image'];
+                      children: filteredCinemasData.isNotEmpty
+                          ? filteredCinemasData.map((cinema) {
+                              final int cinemaId = cinema['id'];
+                              final String name = cinema['name'];
+                              final String address = cinema['endereco'];
+                              final String imageUrl = cinema['image'];
 
-                        return Column(
-                          children: [
-                            Row(
-                              children: [
-                                Container(
-                                  width: 105,
-                                  height: 105,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(200),
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color: const Color.fromARGB(45, 0, 0, 0)
-                                            .withOpacity(0.1),
-                                        spreadRadius: 2,
-                                        blurRadius: 2,
-                                        offset: const Offset(2, 2),
-                                      ),
-                                    ],
-                                  ),
-                                  child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(6),
-                                    child: Image.network(
-                                      imageUrl,
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                ),
-                                const SizedBox(
-                                  width: 15,
-                                ),
-                                Expanded(
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
+                              return Column(
+                                children: [
+                                  Row(
                                     children: [
-                                      Text(
-                                        name,
-                                        style: const TextStyle(
-                                          fontFamily: 'Inter',
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.w700,
-                                          color: Color(0xFFD9D9D9),
-                                        ),
-                                      ),
-                                      const SizedBox(
-                                        height: 5,
-                                      ),
-                                      Text(
-                                        address,
-                                        softWrap: true,
-                                        maxLines: 2,
-                                        style: const TextStyle(
-                                          fontFamily: 'Inter',
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.w400,
-                                          color: Color(0xFFD9D9D9),
-                                        ),
-                                      ),
-                                      const SizedBox(
-                                        height: 5,
-                                      ),
-                                      SizedBox(
-                                        width: double.infinity,
-                                        child: ElevatedButton(
-                                          style: ElevatedButton.styleFrom(
-                                            backgroundColor:
-                                                selectedCinemaId == cinemaId
-                                                    ? const Color(0xFF590A0A)
-                                                    : const Color(0xFF111111),
-                                            side: const BorderSide(
-                                              width: 2,
-                                              color: Color(0xFF590A0A),
+                                      Container(
+                                        width: 105,
+                                        height: 105,
+                                        decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(200),
+                                          boxShadow: [
+                                            BoxShadow(
+                                              color: const Color.fromARGB(
+                                                      45, 0, 0, 0)
+                                                  .withOpacity(0.1),
+                                              spreadRadius: 2,
+                                              blurRadius: 2,
+                                              offset: const Offset(2, 2),
                                             ),
+                                          ],
+                                        ),
+                                        child: ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(6),
+                                          child: Image.network(
+                                            imageUrl,
+                                            fit: BoxFit.cover,
                                           ),
-                                          onPressed: () {
-                                            selectCinema(cinemaId);
-                                          },
-                                          child: Text(
-                                            selectedCinemaId == cinemaId
-                                                ? 'Selecionado'
-                                                : 'Selecionar',
-                                          ),
+                                        ),
+                                      ),
+                                      const SizedBox(
+                                        width: 15,
+                                      ),
+                                      Expanded(
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              name,
+                                              style: const TextStyle(
+                                                fontFamily: 'Inter',
+                                                fontSize: 16,
+                                                fontWeight: FontWeight.w700,
+                                                color: Color(0xFFD9D9D9),
+                                              ),
+                                            ),
+                                            const SizedBox(
+                                              height: 5,
+                                            ),
+                                            Text(
+                                              address,
+                                              softWrap: true,
+                                              maxLines: 2,
+                                              style: const TextStyle(
+                                                fontFamily: 'Inter',
+                                                fontSize: 14,
+                                                fontWeight: FontWeight.w400,
+                                                color: Color(0xFFD9D9D9),
+                                              ),
+                                            ),
+                                            const SizedBox(
+                                              height: 5,
+                                            ),
+                                            if (selectedCinemaId != null &&
+                                                selectedCinemaId == cinemaId)
+                                              SizedBox(
+                                                width: double.infinity,
+                                                child: ElevatedButton(
+                                                  style:
+                                                      ElevatedButton.styleFrom(
+                                                    backgroundColor:
+                                                        const Color(0xFF590A0A),
+                                                  ),
+                                                  onPressed: () {},
+                                                  child:
+                                                      const Text('Selecionado'),
+                                                ),
+                                              )
+                                            else
+                                              SizedBox(
+                                                width: double.infinity,
+                                                child: ElevatedButton(
+                                                  style:
+                                                      ElevatedButton.styleFrom(
+                                                    backgroundColor:
+                                                        const Color(0xFF111111),
+                                                    side: const BorderSide(
+                                                      width: 2,
+                                                      color: Color(0xFF590A0A),
+                                                    ),
+                                                  ),
+                                                  onPressed: () {
+                                                    selectCinema(cinemaId);
+                                                  },
+                                                  child:
+                                                      const Text('Selecionar'),
+                                                ),
+                                              ),
+                                          ],
                                         ),
                                       ),
                                     ],
                                   ),
+                                  const SizedBox(
+                                    height: 30,
+                                  ),
+                                ],
+                              );
+                            }).toList()
+                          : [
+                              const Text(
+                                'Nenhum cinema encontrado.',
+                                style: TextStyle(
+                                  fontFamily: 'Inter',
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w700,
+                                  color: Color.fromARGB(82, 217, 217, 217),
                                 ),
-                              ],
-                            ),
-                            const SizedBox(
-                              height: 30,
-                            ),
-                          ],
-                        );
-                      }).toList(),
+                                textAlign: TextAlign.center,
+                              ),
+                            ],
                     ),
                   ],
                 ),
